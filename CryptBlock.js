96d4253f1315626e34bba7fd20ed6b54d390bc0a
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2022-04-29 22:49:46 +0530

    Update CryptBlock.js

--- CryptBlock.js
+++ CryptBlock.js
@@ -1,14 +1,18 @@
+
 const SHA256 = require('crypto-js/sha256')
+
+
 
 const DIFFICULTY = 3
 
 class CryptBlock{
+    
     constructor(timestamp, data, nodeHash, prevHash = "", index = 0, nonce=0, hash = ""){
         this.index = index
         this.timestamp = timestamp
-        this.data = data
         this.nodeHash = nodeHash
         this.prevHash = prevHash
+        this.data = data
         this.nonce = nonce || 0
         this.hash = (hash == "")?this.computeHash():hash
     }    
@@ -20,7 +24,6 @@ class CryptBlock{
         }
         //this.nonce = 0
     }
-
     computeHash(){
         return SHA256(this.index + this.timestamp + JSON.stringify(this.data) + this.nodeHash + this.prevHash + this.nonce).toString()
     }
